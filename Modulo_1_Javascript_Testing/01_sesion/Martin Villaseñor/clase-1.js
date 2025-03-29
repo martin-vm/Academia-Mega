1a2a7ccaaa1f46f8abfcd1104dddf49cbf4a3887
martin-vm <mvillasenor@megacable.com.mx>
2025-03-28 23:25:20 -0600

    Clase Javascript - Sesion 1

---
+++ Modulo_1_Javascript_Testing/01_sesion/Martin Villaseñor/clase-1.js
@@ -0,0 +1,10 @@
+var int = 12345;
+var string = "texto";
+var boolean = false;
+
+var nombre = "Mart\u00EDn";
+var apellido = "Villase\u00F1or";
+var nombreCompleto = `${nombre} ${apellido}`;
+var edad = 30;
+
+console.log(`Hola, soy ${nombreCompleto} y tengo ${edad} a\u00F1os `);
