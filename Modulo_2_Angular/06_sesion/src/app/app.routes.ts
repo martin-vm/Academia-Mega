71a06b632202780a5bb18385fa9df3b495187631
martin-vm <mvillasenor@megacable.com.mx>
2025-05-19 08:54:17 -0600

    Clase 06

---
+++ Modulo_2_Angular/06_sesion/src/app/app.routes.ts
@@ -0,0 +1,40 @@
+import { Routes } from '@angular/router';
+import { ProductManagerComponent } from './Components/product-manager/product-manager.component';
+import { CardComponent } from './Components/card/card.component';
+import { TodoComponent } from './Components/todo/todo.component';
+import { HomeComponent } from './Page/home/home.component';
+import { ErrorComponent } from './Page/error/error.component';
+import { TarjetaComponent } from './Components/tarjeta/tarjeta.component';
+
+export const routes: Routes = [
+
+    {
+        path: '',
+        component: HomeComponent
+    },    
+    {
+        path: 'gestor',
+        component: ProductManagerComponent
+    },
+    {
+        path:'card',
+        component: CardComponent
+    },
+    {
+        path:'todo',
+        component: TodoComponent
+
+    },
+    {
+        path: 'tarjeta',
+        component: TarjetaComponent
+    },
+    
+    {
+        path:'**',
+        component:ErrorComponent
+    }
+    
+
+
+];
